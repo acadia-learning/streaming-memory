251fba55540681ea7ab445584f50befbd170baec
Bryan Houlton <brhoulton@gmail.com>
2025-12-23 17:58:08 -0500

    fix(posthog): react

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,7 +2,7 @@ import './index.css';
 
 import React from 'react';
 
-import posthog from 'posthog-js';
+import { PostHogProvider } from 'posthog-js/react';
 import ReactDOM from 'react-dom/client';
 import {
   BrowserRouter,
@@ -13,17 +13,22 @@ import {
 import Demo from './Demo';
 import Landing from './Landing';
 
-posthog.init(import.meta.env.VITE_PUBLIC_POSTHOG_KEY, {
+const options = {
   api_host: import.meta.env.VITE_PUBLIC_POSTHOG_HOST,
-});
+};
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Landing />} />
-        <Route path="/demo" element={<Demo />} />
-      </Routes>
-    </BrowserRouter>
+    <PostHogProvider
+      apiKey={import.meta.env.VITE_PUBLIC_POSTHOG_KEY}
+      options={options}
+    >
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Landing />} />
+          <Route path="/demo" element={<Demo />} />
+        </Routes>
+      </BrowserRouter>
+    </PostHogProvider>
   </React.StrictMode>
 );
